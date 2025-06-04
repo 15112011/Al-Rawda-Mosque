97715fe91b4ee5954cdcbe52af8d9f7ff5f6fe3d
hamzasamier1 <hamzasamier1@gmail.com>
2025-06-04 16:03:13 +0300

    Fix merge conflict in postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 module.exports = {
-=======
-export default {
->>>>>>> b6e88818a4e0ae6371cd3b214583b0c9c9f0bced
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-<<<<<<< HEAD
-} 
-=======
 }
->>>>>>> b6e88818a4e0ae6371cd3b214583b0c9c9f0bced
